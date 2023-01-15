5ef640fda31f3bb857353efc422ad72104212948
sean51cyan <yutoman321@icloud.com>
2023-01-15 14:41:12 +0900

    表示する文章を編集

--- app/src/main/java/com/example/dbapplication/TestOpenHelper.java
+++ app/src/main/java/com/example/dbapplication/TestOpenHelper.java
@@ -8,7 +8,7 @@ import android.util.Log;
 
 public class TestOpenHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 7;
+    private static final int DATABASE_VERSION = 6;
 
     private static final String DATABASE_NAME = "kadaiDB.db";
     private static final String TABLE_NAME = "授業";
